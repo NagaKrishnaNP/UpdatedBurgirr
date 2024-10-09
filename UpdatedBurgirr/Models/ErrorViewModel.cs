a43d22c42b24934d1da13fd12d1bfd448f603d73
Naga Krishna N P <nagak915@gmail.com>
2024-10-09 16:20:37 +0530

    Add project files.

---
+++ UpdatedBurgirr/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UpdatedBurgirr.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
